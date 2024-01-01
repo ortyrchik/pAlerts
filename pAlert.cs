37d8764251d02885eec577d931b446906803baf3
ortyrchik <badalyan0552@gmail.com>
2024-01-01 07:41:20 +0100

    Features: Type of Doors, Localization, command: Disconnect
    This commit introduces several significant enhancements to the pAlert plugin:
    
    1. **Door Type Recognition:** The plugin now recognizes and reports the type of doors that were destroyed during raids. Whether it's a Wooden Door, Metal Door, Double Wooden Door, Double Metal Door, Garage Door, Armored Door, or Double Armored Door, users will receive accurate notifications about the type of door that was breached.
    
    2. **Localization Support:** The plugin has been localized to support multiple languages. English and Russian translations have been added, enhancing the user experience for players who prefer different languages.
    
    3. **New Command: /palert disconnect:** A new command, `/palert disconnect`, has been implemented to allow players to unlink their Telegram accounts. This command removes stored verification codes and Telegram IDs, providing users with greater control over their account linkage.
    
    4. **Command List:** Users can now type just `/palert` to receive a list of available commands, making it easier for them to access and understand the plugin's functionality.
    
    There are plans to add square recognition in the next update

--- pAlert.cs
+++ pAlert.cs
@@ -2,28 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using Newtonsoft.Json;
 using Oxide.Core;
-
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Game.Rust.Cui;
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("pAlert", "promise", "1.0.0")]
+    [Info("pAlert", "promise", "1.0.1")]
     [Description("Alert telegram when raid")]
     class pAlert : RustPlugin
     {
+
+        #region data
         private string BotToken;
         private Dictionary<string, string> UserTelegramIDs;
         private Dictionary<string, string> VerificationCodes;
         private Dictionary<string, string> ActiveVerificationCodes = new Dictionary<string, string>();
-
+        private string CurrentLanguage;
 
         protected override void LoadDefaultConfig()
         {
             if (Config["BotToken"] == null)
             {
                 Config.Set("BotToken", "ваш_токен");
-                SaveConfig();
             }
+            if (Config["Language"] == null)
+            {
+                Config.Set("Language", "en");
+            }
+            SaveConfig();
+            CurrentLanguage = Config.Get<string>("Language");
+
         }
 
         private T GetConfig<T>(string key, T defaultValue)
@@ -37,7 +48,9 @@ namespace Oxide.Plugins
         {
             LoadDefaultConfig();
             LoadData();
+            LoadDefaultMessages();
         }
+
 
         private void LoadData()
         {
@@ -53,7 +66,7 @@ namespace Oxide.Plugins
                 VerificationCodes = new Dictionary<string, string>();
             }
 
-            // Теперь устанавливаем токен из конфига только если он отсутствует
+
             if (Config["BotToken"] == null)
             {
                 Config.Set("BotToken", "ваш_токен");
@@ -71,6 +84,79 @@ namespace Oxide.Plugins
             Interface.Oxide.DataFileSystem.WriteObject("pAlert_verificationCodes", VerificationCodes);
         }
 
+        #endregion
+        #region local
+
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["WoodenDoor"] = "Wooden Door",
+                ["MetalDoor"] = "Metal Door",
+                ["DoubleWoodenDoor"] = "Double Wooden Door",
+                ["DoubleMetalDoor"] = "Double Metal Door",
+                ["GarageDoor"] = "Garage Door",
+                ["ArmoredDoor"] = "Armored Door",
+                ["DoubleArmoredDoor"] = "Double Armored Door",
+                ["UnknownDoor"] = "Unknown Door",
+                ["DoorDestroyed"] = "{0} destroyed your {1} at {2}.",
+                ["ObjectDestroyed"] = "{0} destroyed your structure: quality {1} ({2}) at coordinates {3}",
+                ["ConnectUsage"] = "Usage: /connect [TG ID]",
+                ["CheckUsage"] = "Usage: /check [Code]",
+                ["TelegramAlreadyLinked"] = "Your account is already linked to Telegram.",
+                ["TelegramLinkSuccess"] = "Your account has been successfully linked to Telegram ID.",
+                ["ErrorLinkingTelegram"] = "Error: Your Telegram account was not previously linked. Use /connect [TG ID]",
+                ["CodeMismatch"] = "Incorrect code. Please make sure you entered the correct code from the Telegram message.",
+                ["CodeUsedOrNotSent"] = "Error: This code has already been used or you were not sent a code.",
+                ["endConnect"] = "A code has been sent to you on Telegram. To complete the linking, enter the command: /check Code",
+                ["fiveN"] = "Error: The code must be 5 digits long.",
+                ["Disconnect"] = "Your Telegram account has been successfully disconnected.",
+                ["noConnect"] = "Your account is not linked to any Telegram ID."
+            }, this);
+
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["WoodenDoor"] = "Деревянная дверь",
+                ["MetalDoor"] = "Железная дверь",
+                ["DoubleWoodenDoor"] = "Двойная деревянная дверь",
+                ["DoubleMetalDoor"] = "Двойная железная дверь",
+                ["GarageDoor"] = "Гаражная дверь",
+                ["ArmoredDoor"] = "Бронированная дверь",
+                ["DoubleArmoredDoor"] = "Двойная бронированная дверь",
+                ["UnknownDoor"] = "Неизвестная дверь",
+                ["DoorDestroyed"] = "{0} разрушил вашу {1} в вашем строении на координатах {2}.",
+                ["ObjectDestoyed"] = "{0} разрушил ваше строение: качество {1} ({2}) на координатах {3}",
+                ["ConnectUsage"] = "Использование: /connect [ТГ ID]",
+                ["CheckUsage"] = "Использование: /check [код]",
+                ["TelegramAlreadyLinked"] = "Ваш аккаунт уже привязан к Телеграм.",
+                ["TelegramLinkSuccess"] = "Ваш аккаунт успешно привязан к Telegram ID.",
+                ["ErrorLinkingTelegram"] = "Ошибка: Ваш телеграм аккаунт не был предварительно привязан. Используйте /connect [ТГ ID]",
+                ["CodeMismatch"] = "Неверный код. Пожалуйста, убедитесь, что вы ввели правильный код из сообщения в Телеграм.",
+                ["CodeUsedOrNotSent"] = "Ошибка: Этот код уже использован или вам не был отправлен код.",
+                ["endConnect"] = "Вам был отправлен код в Telegram. Для завершения привязки введите команду: /check Код",
+                ["fiveN"] = "Ошибка: Код должен быть 5-значным.",
+                ["Disconnect"] = "Ваш телеграм аккаунт был успешно отвязан!",
+                ["noConnect"] = "Ваш аккаунт не привязан к телеграм аккаунту!."
+
+
+            }, this, "ru");
+        }
+
+        private string GetMessage(string key, string userId = null)
+        {
+            Dictionary<string, string> messages = lang.GetMessages(CurrentLanguage, this);
+            if (messages.TryGetValue(key, out string message))
+            {
+                return message;
+            }
+            else
+            {
+                return $"[Missing Translation: {key}]";
+            }
+        }
+        #endregion
+        #region xz
         private void SendTelegramMessage(string chatId, string message)
         {
 
@@ -95,6 +181,24 @@ namespace Oxide.Plugins
             }
         }
 
+        private string GetDoorDescription(string shortPrefabName, string userId)
+        {
+            string key = shortPrefabName switch
+            {
+                "door.hinged.wood" => "WoodenDoor",
+                "door.hinged.metal" => "MetalDoor",
+                "door.double.hinged.wood" => "DoubleWoodenDoor",
+                "door.double.hinged.metal" => "DoubleMetalDoor",
+                "wall.frame.garagedoor" => "GarageDoor",
+                "door.hinged.toptier" => "ArmoredDoor",
+                "door.double.hinged.toptier" => "DoubleArmoredDoor",
+                _ => "UnknownDoor"
+            };
+
+            return lang.GetMessage(key, this, userId);
+        }
+        #endregion
+        #region hooks
         private void OnEntityDeath(BaseCombatEntity entity, HitInfo hitInfo)
         {
             if (entity == null || hitInfo == null || hitInfo.InitiatorPlayer == null)
@@ -120,11 +224,10 @@ namespace Oxide.Plugins
                     // солому нахрен
                     if (!string.Equals(buildingGrade, "twigs", StringComparison.OrdinalIgnoreCase))
                     {
-                        // Puts($"{attacker.displayName} ({attackerSteamID}) разрушил {buildingGrade} {entityType} игрока {owner?.displayName ?? "unknown"} ({ownerSteamID}) на координатах {entityPosition}");
-
                         if (owner != null && UserTelegramIDs.TryGetValue(owner.UserIDString, out string chatId))
                         {
-                            string message = $"{attacker.displayName} разрушил ваше строение: качество {buildingGrade} ({entityType}) на координатах {entityPosition}";
+
+                            string message = string.Format(GetMessage("ObjectDestoyed"), attacker.displayName, buildingGrade, entityType, entityPosition);
                             SendTelegramMessage(chatId, message);
                         }
                     }
@@ -132,30 +235,30 @@ namespace Oxide.Plugins
             }
 
             // дверь
-            if (entityType.IndexOf("door", StringComparison.OrdinalIgnoreCase) >= 0)
+
+            if (entityType.Contains("door", StringComparison.OrdinalIgnoreCase))
             {
-                // сам себя рейдит
+                string doorType = GetDoorDescription(entity.ShortPrefabName, attacker.UserIDString);
+
                 if (owner != null && attacker != owner)
                 {
-                    // Puts($"{attacker.displayName} ({attackerSteamID}) разрушил дверь игрока {owner?.displayName ?? "unknown"} ({ownerSteamID})");
                     if (owner != null && UserTelegramIDs.TryGetValue(owner.UserIDString, out string chatId))
                     {
                         string entityPosition = entity.transform.position.ToString();
-                        string message = $"{attacker.displayName} разрушил дверь в вашем строении на координатах {entityPosition}.";
+                        string message = string.Format(GetMessage("DoorDestroyed"), attacker.displayName, doorType, entityPosition);
                         SendTelegramMessage(chatId, message);
                     }
                 }
             }
-
         }
-
-
+        #endregion
+        #region commands
         [ChatCommand("connect")]
         private void ConnectCommand(BasePlayer player, string command, string[] args)
         {
             if (args.Length != 1)
             {
-                SendReply(player, "[pAlert] Используйте: /connect [ТГ Айди] (@getmyid_bot)");
+                SendReply(player, GetMessage("ConnectUsage", player.UserIDString));
                 return;
             }
 
@@ -164,36 +267,33 @@ namespace Oxide.Plugins
 
             if (UserTelegramIDs.ContainsKey(steamId))
             {
-                SendReply(player, "[pAlert] К вашему аккаунту уже привязан телеграм.");
+                SendReply(player, GetMessage("TelegramAlreadyLinked", player.UserIDString));
                 return;
             }
 
             if (ActiveVerificationCodes.ContainsKey(telegramId))
             {
-                SendReply(player, "[pAlert] Код уже отправлен другому игроку.");
+                SendReply(player, GetMessage("CodeUsedOrNotSent", player.UserIDString));
                 return;
             }
 
-            // рандом код
             string randomCode = GenerateRandomCode();
             SendTelegramMessage(telegramId, $"[pAlert] Для завершения привязки введите команду: /check {randomCode}");
-
 
             UserTelegramIDs[steamId] = telegramId;
             ActiveVerificationCodes[telegramId] = steamId;
             VerificationCodes[telegramId] = randomCode;
             SaveData();
 
-            SendReply(player, $"[pAlert] Вам был отправлен код в Telegram. Для завершения привязки введите команду: /check [Код]");
+            SendReply(player, GetMessage("endConnect", player.UserIDString));
         }
-
 
         [ChatCommand("check")]
         private void CheckCommand(BasePlayer player, string command, string[] args)
         {
             if (args.Length != 1)
             {
-                SendReply(player, "[pAlert] Используйте: /check [код]");
+                SendReply(player, GetMessage("CheckUsage", player.UserIDString));
                 return;
             }
 
@@ -202,13 +302,13 @@ namespace Oxide.Plugins
 
             if (!UserTelegramIDs.TryGetValue(steamId, out string telegramId))
             {
-                SendReply(player, "[pAlert] Ошибка: Ваш телеграм аккаунт не был предварительно привязан. Используйте команду /connect [ТГ Айди] (@getmyid_bot)");
+                SendReply(player, GetMessage("ErrorLinkingTelegram", player.UserIDString));
                 return;
             }
 
             if (code.Length != 5)
             {
-                SendReply(player, "[pAlert] Ошибка: Код должен быть 5-значным.");
+                SendReply(player, GetMessage("fiveN", player.UserIDString));
                 return;
             }
 
@@ -216,25 +316,61 @@ namespace Oxide.Plugins
             {
                 if (VerificationCodes.TryGetValue(telegramId, out string randomCode) && randomCode == code)
                 {
-                    // Код совпал, привязываем Steam ID к Telegram ID
+
                     ActiveVerificationCodes.Remove(telegramId);
                     VerificationCodes.Remove(telegramId);
                     SaveData();
-                    SendReply(player, "[pAlert] Ваш аккаунт успешно привязан к Telegram ID.");
+                    SendReply(player, GetMessage("TelegramLinkSuccess", player.UserIDString));
                 }
                 else
                 {
-                    SendReply(player, "[pAlert] Неверный код. Пожалуйста, убедитесь, что вы ввели правильный код из сообщения в Telegram.");
+                    SendReply(player, GetMessage("CodeMismatch", player.UserIDString));
                 }
             }
             else
             {
-                SendReply(player, "[pAlert] Ошибка: Этот код уже использован или вам не был отправлен код.");
+                SendReply(player, GetMessage("CodeUsedOrNotSent", player.UserIDString));
             }
+        }
+
+        [ChatCommand("disconnect")]
+        private void DisconnectCommand(BasePlayer player, string command, string[] args)
+        {
+            string steamId = player.UserIDString;
+
+            if (UserTelegramIDs.ContainsKey(steamId))
+            {
+                UserTelegramIDs.Remove(steamId);
+                ActiveVerificationCodes.Remove(steamId);
+                SaveData();
+
+
+                SendReply(player, GetMessage("Disconnect", player.UserIDString));
+            }
+            else
+            {
+                SendReply(player, GetMessage("noConnect", player.UserIDString));
+            }
+        }
+
+        [ChatCommand("palert")]
+        private void PAlertCommand(BasePlayer player, string command, string[] args)
+        {
+            if (args.Length == 0)
+            {
+                SendReply(player, "Available commands:");
+                SendReply(player, "/connect [Telegram ID] - Connect your Telegram account.");
+                SendReply(player, "/check [Code] - Check and complete the Telegram account linking.");
+                SendReply(player, "/disconnect - Disconnect your Telegram account.");
+                return;
+            }
+
+
         }
 
 
 
+        #endregion
 
 
         private string GenerateRandomCode()
